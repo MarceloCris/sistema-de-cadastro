eb5b58ce3b6847dc7b7d2fb9cb23880982565efd
MarceloCris <149599863+MarceloCris@users.noreply.github.com>
2023-11-08 13:41:14 -0300

    voltando para o estado do Paulo

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("rodando o sistema de gerenciamento de jogos") 
-consgole.log("aplicação esta se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O Paulo fez esse commit para ver na aula!")
-console.log("Enviando para o Github")
